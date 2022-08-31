0c751ac96e7135fd142a8a2762683902d365e375
gprandamnjanovic <damnjanovicgoran7@yahoo.com>
2022-08-31 13:56:44 +0200

    cors-origin

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -4,6 +4,9 @@ const items = require('../assets/data');
 
 exports.handler = async (event, context) => {
   return {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
     statusCode: 200,
     body: JSON.stringify(items),
   };
